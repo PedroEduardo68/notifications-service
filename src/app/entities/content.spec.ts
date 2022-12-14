229bb206039f4a80e8dcfff777c5c86be4e1c346
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2022-12-13 23:01:45 -0300

    jest frist

--- src/app/entities/content.spec.ts
+++ src/app/entities/content.spec.ts
@@ -5,3 +5,11 @@ test('it should be able to create a notification content', () => {
 
   expect(content).toBeTruthy();
 });
+
+test('it should not be able to create a notification content', () => {
+  expect(() => new Content('aaa')).toThrow();
+});
+
+test('it should not be able to create a notification content', () => {
+  expect(() => new Content('a'.repeat(241))).toThrow();
+});

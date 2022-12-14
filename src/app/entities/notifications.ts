b32d33500eda14db2e196e0e1d6ba944ea3258ff
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2022-12-13 22:59:01 -0300

    test jest in js

--- src/app/entities/notifications.ts
+++ src/app/entities/notifications.ts
@@ -1,6 +1,8 @@
+import { Content } from './content';
+
 export interface NotificationProps {
   recipientId: string;
-  content: string;
+  content: Content;
   category: string;
   readAt?: Date | null;
   createdAt: Date;
@@ -13,15 +15,39 @@ export class Notification {
     this.props = props;
   }
 
-  public set content(content: string) {
-    if (content.length < 5) {
-      throw new Error();
-    }
-
+  public set content(content: Content) {
     this.props.content = content;
   }
 
-  public get content(): string {
+  public get content(): Content {
     return this.props.content;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
   }
 }

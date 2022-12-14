753f28384939fe48b5d795da060f247fe086824e
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2022-12-13 23:03:12 -0300

    jest second

--- src/app/entities/content.spec.ts
+++ src/app/entities/content.spec.ts
@@ -1,15 +1,17 @@
 import { Content } from './content';
 
-test('it should be able to create a notification content', () => {
-  const content = new Content('You recever a friends notifications');
+describe('Notification content', () => {
+  it('it should be able to create a notification content', () => {
+    const content = new Content('You recever a friends notifications');
 
-  expect(content).toBeTruthy();
-});
+    expect(content).toBeTruthy();
+  });
 
-test('it should not be able to create a notification content', () => {
-  expect(() => new Content('aaa')).toThrow();
-});
+  it('it should not be able to create a notification content', () => {
+    expect(() => new Content('aaa')).toThrow();
+  });
 
-test('it should not be able to create a notification content', () => {
-  expect(() => new Content('a'.repeat(241))).toThrow();
+  it('it should not be able to create a notification content', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
 });
